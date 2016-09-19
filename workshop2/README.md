a77be12f08da3aa008b788bf567a8e3d4cf201d9
gk222gs <gk222gs@student.lnu.se>
2016-09-18 22:36:22 -0700

    Adding runnable jar file and instructions on how to run the application.

---
+++ workshop2/README.md
@@ -0,0 +1,5 @@
+# 1DV607 - workshop 2
+Run this project with the jar file workshop2-dk222gs.jar.
+Grade 2 done.
+
+``java -jar workshop2-dk222gs.jar``
--- workshop2/src/com/lnu/workshop2/controller/CommonProperties.java
+++ workshop2/src/com/lnu/workshop2/controller/CommonProperties.java
@@ -2,6 +2,6 @@ package com.lnu.workshop2.controller;
 
 public class CommonProperties {
 
-	public final static String serializableFilePath = "./resources/memberlist.ser";
+	public final static String serializableFilePath = "./memberlist.ser";
 	
 }
